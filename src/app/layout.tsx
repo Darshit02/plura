8665b5d521bd8101abcc1e31cd0292b4f06a00f1
LazY <darshitmistry2@gmail.com>
2024-05-26 19:18:25 +0530

    style : landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import {
   ClerkProvider,
 } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
---
+++ src/app/site/layout.tsx
@@ -0,0 +1,19 @@
+import Navigation from '@/components/site/navigation'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import React from 'react'
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    // <ClerkProvider appearance={{ baseTheme: dark }}>
+    <>
+        <Navigation />
+     
+        {children}
+    </>
+    
+    // </ClerkProvider>
+  )
+}
+
+export default layout
