e4f52c345b74ff8801b5168f683117eb2231c03f
DevendraBhoraniya <bhoraniyadevendra@gmail.com>
2024-06-10 21:01:09 +0530

    Create: editor-provider , editor-actions & funnel-editor-navigation

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/page.tsx
@@ -1,25 +1,26 @@
-import BlurePage from '@/components/global/blure-page'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { getFunnel } from '@/lib/query'
-import Link from 'next/link'
-import { redirect } from 'next/navigation'
-import React from 'react'
-import FunnelSteps from './_components/funnel-step'
-import FunnelSettings from './_components/funnel-settings'
+import BlurePage from "@/components/global/blure-page";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getFunnel } from "@/lib/query";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
+import FunnelSteps from "./_components/funnel-step";
+import FunnelSettings from "./_components/funnel-settings";
 
 type Props = {
-    params : {
-        funnelId: string
-        subaccountId: string
-    }
-}
+  params: {
+    funnelId: string;
+    subaccountId: string;
+  };
+};
 
-const page = async ({
-params
-}: Props) => {
-    const funnelPages = await getFunnel(params.funnelId)
-    if (!funnelPages)
-        return redirect(`/subaccount/${params.subaccountId}/funnels`)
+const page = async ({ params }: Props) => {
+
+
+  const funnelPages = await getFunnel(params.funnelId);
+  if (!funnelPages)
+    return redirect(`/subaccount/${params.subaccountId}/funnels`);
+
   return (
     <BlurePage>
       <Link
@@ -29,10 +30,7 @@ params
         Back
       </Link>
       <h1 className="text-3xl mb-8">{funnelPages.name}</h1>
-      <Tabs
-        defaultValue="steps"
-        className="w-full"
-      >
+      <Tabs defaultValue="steps" className="w-full">
         <TabsList className="grid  grid-cols-2 w-[50%] bg-transparent ">
           <TabsTrigger value="steps">Steps</TabsTrigger>
           <TabsTrigger value="settings">Settings</TabsTrigger>
@@ -53,7 +51,7 @@ params
         </TabsContent>
       </Tabs>
     </BlurePage>
-  )
-}
+  );
+};
 
-export default page
+export default page;
