07392f6c45830930be7b81124bfb3fb9a69766d8
Darshit Mistry <darshitmistry2@gmail.com>
2024-06-12 09:32:57 +0530

    fix : editor sidebar issue

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/index.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor-sidebar/tabs/index.tsx
@@ -1,85 +1,38 @@
-'use client'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from '@/components/ui/sheet'
-import { Tabs, TabsContent } from '@/components/ui/tabs'
-import clsx from 'clsx'
 import React from 'react'
-import TabList from '.'
-import { useEditor } from '@/providers/editor-provider'
-import SettingsTab from './settings-tab'
-import MediaBucketTab from './media-bucket-tab'
-import ComponentsTab from './components-tabs'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Database, Plus, SettingsIcon, SquareStackIcon } from 'lucide-react'
 
-type Props = {
-  subaccountId: string
-}
+type Props = {}
 
-const FunnelEditorSidebar = ({ subaccountId }: Props) => {
-  const { state, dispatch } = useEditor()
-
+const TabList = (props: Props) => {
   return (
-    <Sheet
-      open={true}
-      modal={false}
-    >
-      <Tabs
-        className="w-full "
-        defaultValue="Settings"
+    <TabsList className=" flex items-center flex-col justify-evenly w-full bg-transparent h-fit gap-4 ">
+      <TabsTrigger
+        value="Settings"
+        className="w-10 h-10 p-0 data-[state=active]:bg-muted"
       >
-        <SheetContent
-          showX={false}
-          side="right"
-          className={clsx(
-            'mt-[97px] w-16 z-[80] shadow-none  p-0 focus:border-none transition-all overflow-hidden',
-            { hidden: state.editor.previewMode }
-          )}
-        >
-          <TabList 
-          subaccountId={subaccountId}
-          key={subaccountId}
-          />
-        </SheetContent>
-        <SheetContent
-          showX={false}
-          side="right"
-          className={clsx(
-            'mt-[97px] w-80 z-[40] shadow-none p-0 mr-16 bg-background h-full transition-all overflow-hidden ',
-            { hidden: state.editor.previewMode }
-          )}
-        >
-          <div className="grid gap-4 h-full pb-36 overflow-scroll">
-            <TabsContent value="Settings">
-              <SheetHeader className="text-left p-6">
-                <SheetTitle>Styles</SheetTitle>
-                <SheetDescription>
-                  Show your creativity! You can customize every component as you
-                  like.
-                </SheetDescription>
-              </SheetHeader>
-              <SettingsTab />
-            </TabsContent>
-            <TabsContent value="Media">
-              <MediaBucketTab subaccountId={subaccountId} />
-            </TabsContent>
-            <TabsContent value="Components">
-              <SheetHeader className="text-left p-6 ">
-                <SheetTitle>Components</SheetTitle>
-                <SheetDescription>
-                  You can drag and drop components on the canvas
-                </SheetDescription>
-              </SheetHeader>
-              <ComponentsTab />
-            </TabsContent>
-          </div>
-        </SheetContent>
-      </Tabs>
-    </Sheet>
+        <SettingsIcon />
+      </TabsTrigger>
+      <TabsTrigger
+        value="Components"
+        className="data-[state=active]:bg-muted w-10 h-10 p-0"
+      >
+        <Plus />
+      </TabsTrigger>
+      <TabsTrigger
+        value="Layers"
+        className="w-10 h-10 p-0 data-[state=active]:bg-muted"
+      >
+        <SquareStackIcon />
+      </TabsTrigger>
+      <TabsTrigger
+        value="Media"
+        className="w-10 h-10 p-0 data-[state=active]:bg-muted"
+      >
+        <Database />
+      </TabsTrigger>
+    </TabsList>
   )
 }
 
-export default FunnelEditorSidebar
+export default TabList
