42dcba721c05c300597a94a8139ecca84cb556fb
Darshit Mistry <darshitmistry2@gmail.com>
2024-06-15 10:30:21 +0530

    feat : domain hoasted

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,8 +1,40 @@
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
 import React from 'react'
+import FunnelEditor from '../(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor'
+import { getDomainContent } from '@/lib/query'
+import EditorProvider from '@/providers/editor-provider'
 
-const Page = () => {
+const Page = async ({ params }: { params: { domain: string } }) => {
+  const domainData = await getDomainContent(params.domain.slice(0, -1))
+  if (!domainData) return notFound()
+
+  const pageData = domainData.FunnelPages.find((page) => !page.pathName)
+
+  if (!pageData) return notFound()
+
+  await db.funnelPage.update({
+    where: {
+      id: pageData.id,
+    },
+    data: {
+      visits: {
+        increment: 1,
+      },
+    },
+  })
+
   return (
-    <div>Page</div>
+    <EditorProvider
+      subaccountId={domainData.subAccountId}
+      pageDetails={pageData}
+      funnelId={domainData.id}
+    >
+      <FunnelEditor
+        funnelPageId={pageData.id}
+        liveMode={true}
+      />
+    </EditorProvider>
   )
 }
 
